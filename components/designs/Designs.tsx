dca3d95df71bbc0e414126b989d436bce100b318
Marcos Casas <maarcoscasas@gmail.com>
2023-05-15 16:08:56 -0300

    fixed design bg-image bug

--- components/designs/Designs.tsx
+++ components/designs/Designs.tsx
@@ -31,7 +31,7 @@ function Designs() {
           </div>
         );
       })}
-      <img className="sqD min-w-[800px] bottom-[-100px] left-1/2 sm:bottom-[-150px] -translate-x-1/2 object-cover sm:min-w-[1100px]" style={{ zIndex: "-10" }} src="/static/doodles/hero/fancyLines.svg" />
+      <img className="sqD md:min-w-[800px] bottom-[-100px] left-1/2 sm:bottom-[-150px] -translate-x-1/2 object-cover sm:min-w-[1100px]" style={{ zIndex: "-10" }} src="/static/doodles/hero/fancyLines.svg" />
     </div>
   );
 }
